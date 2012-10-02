77f98298118de7fb59190b6a5ac510947cdcc226
Jordi Ibáñez <jibanez@userzoom.com>
2012-10-02 09:38:53 +0200

    CHANGE clean of code of update.js

--- update.js
+++ update.js
@@ -1,83 +1,92 @@
 #!/usr/bin/node
 
-//Revisar: https://github.com/zma/usefulscripts/blob/master/script/post-receive
+//Node script to send announcements from VREF gitolite server to Naidbot @aelinformatico
 
-var sys = require('util');
-var net = require('net');
-var exec = require('child_process').exec;
+//TODO: Review: https://github.com/zma/usefulscripts/blob/master/script/post-receive
 
-var newbranch = (process.argv[3] == 0) ;
-
-var debug = false;
-debug = debug? console : { log: function(){} };
-
-//Mensaje base:
-var basemsg = 'g:$GRUPO m:"$MSG"\n';
-var port = 30000; //Poner puerto de Naidbot
-var ip_naidbot = 'localhost'; //Poner IP de NaidBot
-
-// print process.argv
-process.argv.forEach(function (val, index, array) { debug.log(index + ': ' + val); });
-
-//Comando para sacar todos los commits pusheados en este UPDATE.
-var cmd = 'git log --pretty=format:"%h - %an, %ar : %s" '+ process.argv[2]+' ' + process.argv[3]  + '..' + process.argv[4];
-if(newbranch ) {
-  cmd = 'git log --pretty=format:"%H - %an, %ar : %s"  --all  --not $(git branch -a | grep -Fv ' + process.argv[2].replace("refs/heads/","") + ' )';
-  //cmd = 'git log ' + process.argv[2] + ' ';
-  //cmd = 'git rev-list ' + process.argv[4];
-  //cmd = 'git log --pretty=format:"%H %d - %an, %ar : %s" ' + process.argv[4] +'~1..' + process.argv[4];
-  //cmd = 'git rev-list ' + process.argv[5]  + '..' + process.argv[4];
-
-//var cmd = 'git log --pretty=format:"%h - %an, %ar : %s" ' + process.argv[4]  + '..' + process.argv[4];
-var cmd = 'git show --pretty=format:"%h - %an, %ar : %s" ' + process.argv[4] + ' | head -n 1';
-
-}
-debug.log(cmd);
-//Ejecuto el comando que me tiene que dar el resultado a poner en el mensaje del grupo
-var child = exec(cmd, function (error, stdout, stderr) {
-  debug.log('@stdout: ' + stdout);
-  debug.log('@stderr: ' + stderr);
-  if (error !== null) {
-    console.log('@exec error: ' + error);
-  }
-  else {
-    
-    //Consigo el nombre del repositorio
-    var repo = process.cwd().split('/');
-    repo = repo[repo.length-1].replace(".git","");
-
-    var branch = process.argv[2].replace("refs/heads/","");
-    var branchgroup = branch.split('/')[0];
-
-    //Consigo la rama updateada:
-    //Aquí hago el tema de todos los grupos a los que tengo que informar
-    //Inicialmente crearé cuatro grupos
-    //USERZOOM: Siempre estará activo y recibirá todos los mensajes de commit
-    //USERZOOM/repository.git: Solo se enviará cuando se updatea ese repositorio en concreto
-    //USERZOOM/repository.git/rama: Solo se enviará cuando se updatea esa rama de ese repositorio en concreto, la rama es completa ej: USERZOOM/repository.git/fix/ranking-question
-    //USERZOOM/rama: Se enviará cuando se update esa rama para cualquier repositorio.
-    var groups = ['USERZOOM','USERZOOM/'+repo,'USERZOOM/'+repo+'/'+branch,'USERZOOM/'+branchgroup];
-    //Envio los mensaje
-    //Creo los sockets
-    var socket = net.createConnection(port,ip_naidbot,function () {
+({ 
+  //Debug send us verbose output on push event.
+  debug: (false)? console : { log: function(){} },
   
-    });
+  //Requires
+  sys:  require('util'),
+  net:  require('net'),
+  exec: require('child_process').exec,
 
-   socket.on('data',function(data) {
-      debug.log('RESPONSE: ' + data);
-    }).on('connect',function(){
-      var entorno = 'PUSH: ' + repo + ' [' + branch + ']' + (newbranch?' (new branch)':'') + '\r\n';
-  //Envio los mensajes para todos los grupos
-   for (var i = 0;i < groups.length;i++) {
-    var msg = basemsg.replace('$GRUPO', groups[i]).replace("$MSG",entorno + stdout);
-    socket.write(msg);
-   }
-      socket.end();
-    }).on('end',function() {
-      debug.log('DONE');
-    }).on('error',function(e) {
-      console.log('['+ e + '] Servidor Naibot apagado...'); 
-    });
+  // Git command to get git log.
+  cmd: "",
 
+  //Boolean variable, Push created or not a new branch.
+  newbranch: (process.argv[3] == 0),
+
+  //NaidBot configuration
+  
+  naidbot: {
+    basemsg: 'g:$GRUPO m:"$MSG"\n', //Base message to naidbot
+    port: 30000, //NaidBot socket port    
+    ip: 'localhost', //NaidBot socket IP
+  },
+  init: function() {
+    // print process.argv arguments in case of debug activated
+    if(debug) { process.argv.forEach(function (val, index, array) { debug.log(index + ': ' + val); }); }
+    
+    // Git command creation
+    if(updateVREF.newbranch ) { // Git show only last commit information in case of new branch created.
+      this.cmd = 'git show --pretty=format:"%h - %an, %ar : %s" ' + process.argv[4] + ' | head -n 1';
+    }
+    else { // Git show all comits pushed log.
+      this.cmd = 'git log --pretty=format:"%h - %an, %ar : %s" '+ process.argv[2]+' ' + process.argv[3]  + '..' + process.argv[4];
+    }
+    debug.log(cmd);
+  
+    //Send command to system.
+    var child = updateVREF.exec(cmd, function (error, stdout, stderr) {
+      debug.log('@stdout: ' + stdout);
+      debug.log('@stderr: ' + stderr);
+      if (error !== null) {
+        console.log('@exec error: ' + error);
+      }
+      else {    
+        //Get repository name
+        var repo = process.cwd().split('/');
+        repo = repo[repo.length-1].replace(".git","");
+
+        //Get branch name
+        var branch = process.argv[2].replace("refs/heads/","");
+    
+        //Get first diretory of branch.
+        var branchgroup = branch.split('/')[0];
+
+        // Here are all groups to sned infio
+        // Four initial groups
+        // USERZOOM: Get announces from all repositories
+        // USERZOOM/repository.git: Get announces from this specific repository
+        // USERZOOM/repository.git/rama: Get announces from specific repository and branch 
+        // USERZOOM/rama: Get announces from all repositories with this specific branch
+        // var groups = ['USERZOOM','USERZOOM/'+repo,'USERZOOM/'+repo+'/'+branch,'USERZOOM/'+branchgroup];
+    
+        // Initially for testing pouposses only use group USERZOOM
+        var groups = ['USERZOOM'];
+    
+        //Send message to Socket Naidbot.
+        var socket = updateVREF.net.createConnection(updateVREF.naidbot.port,updateVREF.naidbot.ip,function () { });
+        socket.on('data',function(data) {
+            debug.log('RESPONSE: ' + data);
+          }).on('connect',function(){
+            // Header message
+            var entorno = 'PUSH: ' + repo + ' [' + branch + ']' + (updateVREF.newbranch?' (new branch)':'') + '\r\n';
+            // Send the message to all groups
+            for (var i = 0;i < groups.length;i++) {
+              var msg = updateVREF.naidbot.basemsg.replace('$GRUPO', groups[i]).replace("$MSG",entorno + stdout);
+              socket.write(msg);
+            }
+            socket.end();
+          }).on('end',function() {
+              debug.log('Socket closed');
+            }).on('error',function(e) {
+              console.log('['+ e + '] Naidbot server not running, please contact "Oficina de Madrid"...'); 
+            });
+      }
+    });    
   }
-});
+}).init();
